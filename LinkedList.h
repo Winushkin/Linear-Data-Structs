90e1de70acc4d8c7ec42ecb517123ea2b982bbb1
Kirill Belyaev <kirill10121415@gmail.com>
2024-09-25 22:35:12 +0300

    discribe Class LinkedList

--- LinkedList.h
+++ LinkedList.h
@@ -5,8 +5,30 @@
 #ifndef AADT_LAB1_LINKEDLIST_H
 #define AADT_LAB1_LINKEDLIST_H
 
+template <typename T>
+class Node {
+public:
+    T data;
+    Node* prev, *next;
+    explicit Node(T inputData);
+};
 
+
+template <typename T>
 class LinkedList {
+    int length;
+    Node<T> *head = nullptr, *tail = nullptr;
+
+public:
+    LinkedList();
+    int len();
+    void print();
+    void resize();
+    void insert(int index, T value);
+    void append(T value);
+    void pop(int index);
+    void getIndex(T value);
+    void swap(int index1, int index2);
 
 };
 
