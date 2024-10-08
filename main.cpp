fda75163728aca1f90440a76a461a601f3817f9d
Kirill Belyaev <kirill10121415@gmail.com>
2024-10-08 19:05:25 +0300

    files edits

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include "application.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Application app = Application();
+    app.exec();
+
+
     return 0;
 }
